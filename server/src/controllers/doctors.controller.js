2a11aad67be9435fe4d395ddec587af3a68536db
Anjela Depanes <anjeladepanes09@gmail.com>
2025-06-29 19:48:51 +0800

    Add ContactNumber field to GET /doctors response.

--- server/src/controllers/doctors.controller.js
+++ server/src/controllers/doctors.controller.js
@@ -1,7 +1,7 @@
 import db from '../config/db.js';
 
 export function getDoctors(req, res) {
-  const query = `SELECT DoctorID, FirstName, LastName, Specialization FROM doctors`;
+  const query = `SELECT DoctorID, FirstName, LastName, Specialization, ContactNumber FROM doctors`;
   db.query(query, (err, results) => {
     if (err) return res.status(500).json({ error: 'Database error' });
     res.json(results);
